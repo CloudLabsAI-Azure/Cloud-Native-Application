07af0bb0d4b4cfb139468c9728089eba48bf77b9
Sumit Malik <sumitmalik51@gmail.com>
2022-12-09 01:34:44 +0530

    Update logontask01.ps1

--- labguide/labfiles/logontask01.ps1
+++ labguide/labfiles/logontask01.ps1
@@ -25,65 +25,11 @@ $password = $AzurePassword
 
 $deploymentid = $env:DeploymentID
 
-$connectionToken = "vcgq2g3sdbfdkywdwvdzylugpr3dtymx22aybvo66vp5esgdccvq"
-
-refreshenv
 
 choco install bicep
 Install-Module Sqlserver -SkipPublisherCheck -Force
 Import-Module Sqlserver
 az config set extension.use_dynamic_install=yes_without_prompt
-
-
-Set-VSTeamAccount -Account https://dev.azure.com/aiw-devops/ -PersonalAccessToken $connectionToken
-
-Add-VSTeamProject -ProjectName contosotraders-$deploymentid -ProcessTemplate Basic 
-
-$project = Get-VSTeamProject -Name contosotraders-$deploymentid
-
-
-
-$projectID = $project.Id
-
-
-Add-VSTeamUserEntitlement -Email $user -ProjectName $project.Name -License Express -LicensingSource none -Group ProjectAdministrator   -Verbose
-
-
-
-$base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($connectionToken)"))
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Basic $base64AuthInfo")
-$headers.Add("Content-Type", "application/json")
-
-
-
-$body = "{
- `"accessLevel`": {
- `"accountLicenseType`": `"express`"
- },
- `"extensions`": [
- {
- `"id`": `"ms.feed`"
- }
- ],
- `"user`": {
- `"principalName`": `"$user`",
- `"subjectKind`": `"user`"
- },
- `"projectEntitlements`": [
- {
- `"group`": {
- `"groupType`": `"projectAdministrator`"
- },
- `"projectRef`": {
- `"id`": `"$projectID`"
- }
- }
- ]
-}"
-
-$response = Invoke-RestMethod 'https://vsaex.dev.azure.com/aiw-devops/_apis/userentitlements?api-version=6.0-preview.3' -Method 'POST' -Headers $headers -Body $body
-$response | ConvertTo-Json
 
 #Download lab files
 cd C:\
@@ -100,7 +46,7 @@ git clone --branch main https://github.com/shivashant25/aiw-devops-with-github-l
 Sleep 5
 
 $path = "C:\Workspaces\lab\aiw-devops-with-github-lab-files\iac"
-(Get-Content -Path "$path\createResources.parameters.json") | ForEach-Object {$_ -Replace "deploymentidvalue", "$DeploymentID"} | Set-Content -Path "$path\createResources.parameters.json"
+(Get-Content -Path "$path\createResources.parameters.json") | ForEach-Object {$_ -Replace "802322", "$DeploymentID"} | Set-Content -Path "$path\createResources.parameters.json"
 
 $path = "C:\Workspaces\lab\aiw-devops-with-github-lab-files\src\ContosoTraders.Ui.Website\src\services"
 (Get-Content -Path "$path\configService.js") | ForEach-Object {$_ -Replace "deploymentidvalue", "$DeploymentID"} | Set-Content -Path "$path\configService.js"
@@ -157,10 +103,13 @@ $server = "contoso-traders-products$deploymentid.database.windows.net"
 az login -u $userName -p  $password
 cd C:\Workspaces\lab\aiw-devops-with-github-lab-files
   
-Invoke-Sqlcmd -InputFile ./src/ContosoTraders.Api.Products/Migration/productsdb.sql -Database productsdb -Username "localadmin" -Password $password -ServerInstance contoso-traders-products802322.database.windows.net -ErrorAction 'Stop' -Verbose -QueryTimeout 1800 # 30min
+Invoke-Sqlcmd -InputFile ./src/ContosoTraders.Api.Products/Migration/productsdb.sql -Database productsdb -Username "localadmin" -Password $password -ServerInstance $server -ErrorAction 'Stop' -Verbose -QueryTimeout 1800 # 30min
 
 
 az aks get-credentials -g $RESOURCE_GROUP_NAME -n $AKS_CLUSTER_NAME
+
+az aks update -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP_NAME --attach-acr /subscriptions/$subscriptionId/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.ContainerRegistry/registries/contosotradersacr$deploymentid
+
 
 az keyvault set-policy -n $KV_NAME --key-permissions get list  --object-id $(az ad user show --id $(az account show --query "user.name" -o tsv) --query "id" -o tsv)
 
@@ -174,64 +123,8 @@ az cdn endpoint purge --no-wait --content-paths '/*' -n $PRODUCTS_CDN_ENDPOINT_N
 
 
 
-kubectl get po -n chaos-testing
-
-choco install kubernetes-helm
-
-helm repo add chaos-mesh https://charts.chaos-mesh.org
-helm repo update
-kubectl create ns chaos-testing
-helm install chaos-mesh chaos-mesh/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
-
-
-kubectl get po -n chaos-testing
-
-kubectl run nginx --image=nginx --restart=Never
-
 
 sleep 20
-
-#check status of docker app installation aand cloned lab files
-$app = Get-Item -Path 'C:\Program Files\Docker\Docker\Docker Desktop.exe' 
-$clonefiles = Get-Item -Path 'C:\Workspaces\lab\aiw-devops-with-github-lab-files\src'
-
-if(($app -ne $null) -and ($clonefiles -ne $null))
-{
-    Write-Information "Validation Passed"
-    
-    $validstatus = "Successfull"
-}
-else {
-    Write-Warning "Validation Failed - see log output"
-    $validstatus = "Failed"
-      }
-      
-Function SetDeploymentStatus($ManualStepStatus, $ManualStepMessage)
-{
-
-    (Get-Content -Path "C:\WindowsAzure\Logs\status-sample.txt") | ForEach-Object {$_ -Replace "ReplaceStatus", "$ManualStepStatus"} | Set-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt"
-    (Get-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt") | ForEach-Object {$_ -Replace "ReplaceMessage", "$ManualStepMessage"} | Set-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt"
-}
- if ($validstatus -eq "Successfull") {
-    $ValidStatus="Succeeded"
-    $ValidMessage="Environment is validated and the deployment is successful"
-    
-Remove-Item 'C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt' -force
-      }
-else {
-    Write-Warning "Validation Failed - see log output"
-    $ValidStatus="Failed"
-    $ValidMessage="Environment Validation Failed and the deployment is Failed"
-      } 
- SetDeploymentStatus $ValidStatus $ValidMessage
-
-#Import Common Functions
-$commonscriptpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0\cloudlabs-common\cloudlabs-windows-functions.ps1"
-. $commonscriptpath
-
-sleep 3
-#Start the cloudlabs agent service 
-CloudlabsManualAgent Start
 
 sleep 5
 Unregister-ScheduledTask -TaskName "Installdocker" -Confirm:$false 
