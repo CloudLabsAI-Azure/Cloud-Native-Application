759f8bb63d5332e4ef199b8bc8a0e0246c70818e
Sumit Malik <sumitmalik51@gmail.com>
2022-12-25 18:52:46 +0530

    Update logontask01.ps1

--- labguide/labfiles/logontask01.ps1
+++ labguide/labfiles/logontask01.ps1
@@ -72,7 +72,7 @@ $RGname = "contosotraders-$deploymentid"
 New-AzResourceGroupDeployment -Name "createresources" -TemplateFile "createResources.bicep" -TemplateParameterFile "createResources.parameters.json" -ResourceGroup $RGname
 
 $AKS_CLUSTER_NAME = "contoso-traders-aks$deploymentid"
-$AKS_NODES_RESOURCE_GROUP_NAME = "contoso-traders-aks-nodes-rg"
+$AKS_NODES_RESOURCE_GROUP_NAME = "contoso-traders-aks-nodes-rg$deploymentid"
 $CDN_PROFILE_NAME = "contoso-traders-cdn$deploymentid"
 $SUB_DEPLOYMENT_REGION = "eastus"
 $KV_NAME = "contosotraderskv$deploymentid"
