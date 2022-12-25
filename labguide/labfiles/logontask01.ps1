36315a70c71481d8b1848087ca654b139b00a5fa
Sumit Malik <sumitmalik51@gmail.com>
2022-12-25 19:36:38 +0530

    Update logontask01.ps1

--- labguide/labfiles/logontask01.ps1
+++ labguide/labfiles/logontask01.ps1
@@ -22,7 +22,7 @@ choco install bicep
 Install-Module Sqlserver -SkipPublisherCheck -Force
 Import-Module Sqlserver
 choco install kubernetes-cli -y
-choco install kubernetes-heml -y
+choco install kubernetes-helm
 az config set extension.use_dynamic_install=yes_without_prompt
 
 #Download lab files
