8fa99749baede8d16c3782003126f192e7f975ad
Sumit Malik <sumitmalik51@gmail.com>
2023-06-08 00:12:05 +0530

    Add files via upload

---
+++ labguide/labfiles/psscript-01 (9).ps1
@@ -0,0 +1,153 @@
+﻿Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+    [string]
+    $AzurePassword,
+    [string]
+    $AzureTenantID,
+    [string]
+    $AzureSubscriptionID,
+    [string]
+    $ODLID,
+    [string]
+    $DeploymentID,
+    [string]
+    $azuserobjectid,
+    [string]
+    $InstallCloudLabsShadow,
+     [string]
+    $AppID,
+
+    [string]
+    $AppSecret,
+    [string]
+    $adminusername,
+
+    [string]
+    $trainerUserName,
+
+
+    [string]
+    $trainerUserPassword
+
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+$adminUsername = "admincontoso"
+[System.Environment]::SetEnvironmentVariable('DeploymentID', $DeploymentID,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('AppID', $AppID,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('AppSecret', $AppSecret,[System.EnvironmentVariableTarget]::Machine)
+
+    $vmAdminUsername = "adminusername"
+  
+    $vmAdminPassword = $AzurePassword
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#Use the commonfunction to install the required files for cloudlabsagent service 
+CloudlabsManualAgent Install
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $azuserobjectid
+InstallChocolatey
+
+sleep 10
+
+
+
+
+sleep  10
+
+#Assign Packages to Install
+choco install vscode
+choco install dotnet-6.0-sdk
+choco install azure-cli
+choco install kubectl -y
+
+sleep  10
+
+
+
+
+sleep 5
+
+#install AZ-module latest version
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Install-Module -Name Az -Force -AllowClobber
+Import-Module -Name Az
+Import-Module Az.Accounts
+
+#Az Login
+
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$subscriptionId = $AzureSubscriptionID
+$TenantID = $AzureTenantID
+
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$Inputstring = $AzureUserName
+$CharArray =$InputString.Split("@")
+$CharArray[1]
+$tenantName = $CharArray[1]
+
+#install git
+choco install git
+
+#install bicep module
+choco install bicep
+Install-Module Sqlserver -SkipPublisherCheck -Force
+Import-Module Sqlserver
+sleep 5
+
+mkdir C:\LABFILES
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/aiw-cloud-native-apps-v2/scripts/logontask.ps1","C:\Packages\logontask.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Cloud-Native-Application/main/labguide/labfiles/web.service.yml","C:\LABFILES\web.service.yml")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/CloudLabsAI-Azure/Cloud-Native-Application/main/labguide/labfiles/web.deployment.yml","C:\LABFILES\web.deployment.yml")
+
+sleep 5
+
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+
+
+
+#Enable Autologon
+$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\admincontoso" -type String  
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value "$vmAdminPassword" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
+
+
+# Scheduled Task to Run PostConfig.ps1 screen on logon
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\$adminUsername" 
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\Packages\logontask.ps1"
+Register-ScheduledTask -TaskName "Installdocker" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+
+#Use the cloudlabs common function to write the status to validation status txt file 
+$Validstatus="Pending"  ##Failed or Successful at the last step
+$Validmessage="Post Deployment is Pending"
+CloudlabsManualAgent setStatus
+
+Stop-Transcript
+Restart-Computer -Force 
